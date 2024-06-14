65390a10f82fa0fb4c1d1785774de6125e08e514
beedabug <bwelch236@gmail.com>
2024-06-13 23:50:33 -0500

    updating

--- glm.py
+++ glm.py
@@ -1,5 +1,3 @@
-# https://engineering.rappi.com/serve-your-first-model-with-scikit-learn-flask-docker-df95efbbd35e
-
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
